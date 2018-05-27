ae5d11e15af7df3e0b6e3d5d51ffa778f7bae2d0
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 23:47:03 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-import flip from "@unction/flip"
-import append from "@unction/append"
-import reduceKeys from "@unction/reducekeys"
-
-import type {KeyedEnumerableType} from "types"
-import type {ArrayType} from "types"
-
-export default function getMany(keyedEnumerable: KeyedEnumerableType): ArrayType<KeyType> {
-  return reduceKeys(flip(append))([])(keyedEnumerable)
+import flip from "@unction/flip";
+import append from "@unction/append";
+import reduceKeys from "@unction/reducekeys";
+export default function getMany(keyedEnumerable) {
+  return reduceKeys(flip(append))([])(keyedEnumerable);
 }
