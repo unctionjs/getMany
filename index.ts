6348ddcd850ae97eadd0f7a37ea9a758cc1d6ace
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-12-29 19:46:36 -0800

    Updating the repository based on a modern look at libraries

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import get from "@unction/get";
 import mapValues from "@unction/mapvalues";
 
 export default function getMany<A, B> (keys: Array<A>) {
-  return function getManyKeys (keyedEnumerable: Array<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string): Array<B> {
-    return mapValues(flip(get)(keyedEnumerable))(keys);
+  return function getManyKeys (keyedEnumerable: Array<B> | Record<string | number | symbol, B> | Map<A, B> | string): Array<B> {
+    return mapValues<B, B>(flip(get)(keyedEnumerable))(keys);
   };
 }
